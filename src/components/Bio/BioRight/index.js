00cac35c4107ab27c29f8f2b572b73667871b3fc
hbhalodia <hit.bhalodia@rtcamp.com>
2022-12-21 15:07:17 +0530

    Remove anchor tag content and added just icons

--- src/components/Bio/BioRight/index.js
+++ src/components/Bio/BioRight/index.js
@@ -1,23 +1,23 @@
 export default function BioRight() {
 	return (
-		<div className="font-HelveticaNeueLTPro-BdCn font-extralight flex flex-col justify-start items-start content-start">
+		<div className="font-HelveticaNeueLTPro-BdCn font-extralight flex flex-row gap-x-8 justify-start items-start content-start">
 			<div className="flex flex-row justify-between items-center">
-				<a target="_blank" rel="noreferrer" href="mailto:hitkumarbhalodia2000@gmail.com">hitkumarbhalodia2000@gmail.com
+				<a target="_blank" rel="noreferrer" href="mailto:hitkumarbhalodia2000@gmail.com">
 					<svg className="inline ml-2 h-5 w-5 text-black"  width="24" height="24" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="3" y="5" width="18" height="14" rx="2" />  <polyline points="3 7 12 13 21 7" /></svg>
 				</a>
 			</div>
 			<div className="flex flex-row justify-between items-center">
-				<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/hit-kumar-bhalodia-a031b9190/">linkedin.com/in/hit-kumar-bhalodia-a031b9190/
+				<a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/hit-kumar-bhalodia-a031b9190/">
 					<svg className="inline ml-2 h-5 w-5 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2} strokeLinecap="round"  strokeLinejoin="round">  <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />  <rect x="2" y="9" width="4" height="12" />  <circle cx="4" cy="4" r="2" /></svg>
 				</a>
 			</div>
 			<div className="flex flex-row justify-between items-center">
-				<a target="_blank" rel="noreferrer" href="https://github.com/hbhalodia">github.com/hbhalodia
+				<a target="_blank" rel="noreferrer" href="https://github.com/hbhalodia">
 					<svg className="inline ml-2 h-5 w-5 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round">  <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
 				</a>
 			</div>
 			<div className="flex flex-row justify-between items-center">
-				<a target="_blank" rel="noreferrer" href="https://twitter.com/hit_bhalodia">@hit_bhalodia
+				<a target="_blank" rel="noreferrer" href="https://twitter.com/hit_bhalodia">
 					<svg className="inline ml-2 h-5 w-5 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2} strokeLinecap="round"  strokeLinejoin="round">  <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" /></svg>
 				</a>
 			</div>
