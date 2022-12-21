8d0fdeec35195ffcfbb29ef37a5d513fbc499e6f
hbhalodia <hit.bhalodia@rtcamp.com>
2022-12-21 15:15:33 +0530

    Add centerised bio on mobile view

--- src/components/Bio/BioLeft/index.js
+++ src/components/Bio/BioLeft/index.js
@@ -2,11 +2,11 @@ import img from "../../../images/bio-image.jpg";
 
 export default function BioLeft() {
 	return (
-		<div className="flex items-start justify-start content-start">
+		<div className="flex items-start justify-start content-start mobile:flex-col mobile:items-center">
 			<div className="mx-4">
 				<img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0 object-cover w-[100px]" src={img} alt="Bio Pic" height={100} width={100} />
 			</div>
-			<div className="font-Tungsten text-Tungston-Bio-font mx-4 my-4">
+			<div className="mobile:text-center font-Tungsten text-Tungston-Bio-font mx-4 my-4">
 				<h3 className="font-semibold">Hit Bhalodia</h3>
 				<h5 className="font-thin">WordPress Engineer</h5>
 			</div>
